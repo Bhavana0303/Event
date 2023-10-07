1f5a8bb89bc0630efa596f7c4a74285dc93c2a9a
Bhavana0303 <140685365+Bhavana0303@users.noreply.github.com>
2023-10-07 08:22:32 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import event,auth,approve
+from fastapi.staticfiles import StaticFiles
+from database import SessionLocal,engine
+import os
+import models
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 
 app=FastAPI()
@@ -19,6 +31,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+models.Base.metadata.create_all(bind=engine)
+
+
+attachments_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "attachments")
+app.mount("/attachments", StaticFiles(directory=attachments_folder_path), name="attachments")
+
 
 app.include_router(auth.router)
 
